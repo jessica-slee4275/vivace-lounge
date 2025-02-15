c7a2fdabd1f50de11e158b93b042acbdfb69e527
jessica-slee4275 <jessica.slee4275@gmail.com>
2025-02-15 00:07:30 -0500

    init commit

---
+++ mysite/home/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),  # 홈페이지
+    path('about/', views.about, name='about'), 
+    path('services/', views.services, name='services'),  
+    path('contact/', views.contact, name='contact'),  
+]
